6ce2a26c8a58fb9961858bcf5769cc641b1068e5
xuyuan <xuyuan@itiger.com>
2019-11-14 00:26:47 +0800

    tests: mod HelloApiTest

--- hello-client/src/test/java/HelloApiTest.java
+++ hello-client/src/test/java/HelloApiTest.java
@@ -6,6 +6,8 @@ import com.twitter.finagle.service.RetryPolicy;
 import com.twitter.finagle.stats.NullStatsReceiver;
 import com.twitter.finagle.thrift.ThriftClientRequest;
 import com.twitter.util.Duration;
+import com.twitter.util.Future;
+import com.twitter.util.FutureEventListener;
 import com.twitter.util.Try;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.junit.Before;
@@ -52,5 +54,32 @@ public class HelloApiTest {
     @Test
     public void testHello() {
         System.out.println(helloService.hello("world").apply());
+    }
+
+    @Test
+    public void testFlatMap() {
+        Future<Integer> futureIntegerA = Future.value(1);
+        Future<Integer> futureIntegerB = futureIntegerA.flatMap(this::multiplyTwo);
+        System.out.println(futureIntegerA.get());
+        System.out.println(futureIntegerB.get());
+    }
+
+    private Future<Integer> multiplyTwo(int i) {
+        return Future.value(i >> 1);
+    }
+
+    @Test
+    public void testCallback() {
+        helloService.ping().addEventListener(new FutureEventListener<String>() {
+            @Override
+            public void onFailure(Throwable cause) {
+                System.out.println(cause);
+            }
+
+            @Override
+            public void onSuccess(String value) {
+                System.out.println(value);
+            }
+        });
     }
 }
